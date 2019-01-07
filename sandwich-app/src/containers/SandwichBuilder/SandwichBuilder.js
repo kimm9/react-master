e2f8e385544ffa66361dc4c4305b9af626ee117c
Matthew Kim <matt.kim712@gmail.com>
2019-01-07 16:26:59 -0500

    new edits

---
+++ sandwich-app/src/containers/SandwichBuilder/SandwichBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux'
+ 
+class SandwichBuilder extends Component {
+	render () {
+		return (
+			<Aux>
+				<div>Sandwich</div>
+				<div>Build Controls</div>
+			</Aux>
+		);
+	}
+}
+
+export default SandwichBuilder; 
